5b6a45acb6116bfc1efd3c150ba19f8de60a02ae
Behrokh Farzad <behrokh79@yahoo.com>
2017-11-08 14:29:27 -0800

    subsets

--- src/ArrayCodes.java
+++ src/ArrayCodes.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayCodes {
 	
 	/*
 	 * Best Time to Buy and Sell Stock I
+	 * 
+	 * TO DO: Best Time to Buy and Sell Stock II
 	 * 
 	 * Say you have an array for which the i-th element is the price of a given stock on day i.
 	 * If you were only permitted to complete at most one transaction (buy one and sell one share of the stock), 
@@ -19,22 +23,53 @@ public class ArrayCodes {
 		for ( int i = 0; i < array.length; i++ ) {
 			maxProfit = Math.max(array[i] - minPrice, maxProfit);
 			minPrice = Math.min(array[i], minPrice);
-			
 		}
 		
 		return maxProfit;
 	}
 	
+	/*
+	 * Subsets
+	 * 
+	 * TO DO: Subsets with having Duplicates
+	 * 
+	 * Given a set of distinct integers, nums, return all possible subsets (the power set).
+	 * Note: The solution set must not contain duplicate subsets.
+	 */
+	public static List<List<Integer>> subsets (int[] array ) {
+		
+		List<List<Integer>> res = new ArrayList<>();
+		List<Integer> temp = new ArrayList<>();
+		
+		subsetsHelper(res, temp, array, 0);
+		return res;
+	}
 	
-	
+	private static void subsetsHelper(List<List<Integer>> res, List<Integer> temp, int[] array, int start) {
+		res.add(new ArrayList<>(temp));
+		
+		for ( int i = start; i < array.length; i++ ) {
+			temp.add(array[i]);
+			subsetsHelper(res, temp, array, i+1);
+			temp.remove(temp.size()-1);
+		}
+	}
+
 	public static void main ( String[] args ) {
 		
 		/*
 		 * Best Time to Buy and Sell Stock I
 		 */
 		int[] array1 = {310, 315, 275, 295, 260, 270, 290, 230, 255, 250};
-		System.out.print(buySellStock(array1));
+		//System.out.print(buySellStock(array1));
+		
+		/*
+		 * Subsets
+		 */
+		int[] array2 = {1, 2, 3};
+		List<List<Integer>> subsetResult = subsets(array2);
+		System.out.println(subsetResult);
 	}
 	
-
+	
 }
