ffdd44e3e6a455c6921c913dc868ddf9accfa3c1
Behrokh Farzad <behrokh79@yahoo.com>
2017-11-08 15:14:05 -0800

    Unique Paths

--- src/ArrayCodes.java
+++ src/ArrayCodes.java
@@ -85,6 +85,35 @@ public class ArrayCodes {
 		}
 	}
 	
+	/*
+	 * Unique Paths
+	 * 
+	 * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+	 * The robot can only move either down or right at any point in time. 
+	 * The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+	 * How many possible unique paths are there?
+	 */
+	public static int uniquePaths ( int row, int col ) {
+		int[][] m = new int[row][col];
+		
+		for ( int i = 0; i < col; i++ ) {
+			m[0][i] = 1;
+		}
+		
+		for ( int j = 0; j < row; j++ ) {
+			m[j][0] = 1;
+		}
+		
+		for ( int i = 1; i < row; i++ ) {
+			for ( int j = 1; j < col; j++ ) {
+				m[i][j] = m[i-1][j] + m[i][j-1];
+			}
+		}
+		return m[row-1][col-1];
+	}
+	
+	
+	
 	//***********************************************************************************************************************
 	//***********************************************************************************************************************
 	//***********************************************************************************************************************
@@ -109,7 +138,14 @@ public class ArrayCodes {
 		 */
 		int[] array3 = {1, 2, 3};
 		List<List<Integer>> permutationResult = permutation(array2);
-		System.out.println(permutationResult);
+		//System.out.println(permutationResult);
+		
+		/*
+		 * Unique Paths
+		 */
+		int row = 7;
+		int col = 3;
+		System.out.println(uniquePaths(row, col));
 		
 		
 	}
